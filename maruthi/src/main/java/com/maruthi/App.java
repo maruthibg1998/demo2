7424b09e7ae9c46fe06cc8695bef57dd3d4885f3
maruthi1998 <maruthi.bg6@gmail.com>
2025-12-03 05:18:09 -0800

    Pushing demo2

---
+++ maruthi/src/main/java/com/maruthi/App.java
@@ -0,0 +1,13 @@
+package com.maruthi;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
